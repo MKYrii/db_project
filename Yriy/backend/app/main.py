1fdb3ca53fc6bb47013cda88a9750de6e7b9d885
Yanochka123 <janorka2004@gmail.com>
2025-05-29 16:02:58 +0300

    docker-compose changed

--- Yriy/backend/app/main.py
+++ Yriy/backend/app/main.py
@@ -1,18 +1,22 @@
-from fastapi import FastAPI
-import os
-from .routes import router
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
 from pathlib import Path
+
+from .routes import router
 
 app = FastAPI()
 
+# Подключаем API роуты
 app.include_router(router, prefix='/api')
 
+# Путь к HTML-файлу
+MAIN_PAGE_PATH = Path(__file__).parent.parent / "frontend" / "main_page.html"
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    if not MAIN_PAGE_PATH.exists():
+        raise HTTPException(status_code=404, detail="HTML file not found")
 
-
-# Подключаем папку frontend с файлом index.html
-app.mount("/", StaticFiles(directory=Path(__file__).parent / "frontend", html=True), name="frontend")
+    with open(MAIN_PAGE_PATH, "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
